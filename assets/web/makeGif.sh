dbe6428a8881de3495e0263ef033bb5fc9d63d36
badadumTss <lucazanny@gmail.com>
2021-05-31 14:43:30 +0200

    better gifs with the new design

--- assets/web/makeGif.sh
+++ assets/web/makeGif.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 ffmpeg -y -i "$1" -vf palettegen palette.png
-ffmpeg -y -i "$1" -i palette.png -filter_complex paletteuse -r 10 "$2"
+ffmpeg -y -i "$1" -i palette.png -filter_complex paletteuse -r 10 "$2" && \
+    rm palette.png && \
+    rm "$1"
